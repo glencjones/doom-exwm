fefc2919a5bdbdfe87b6786133667fae0f175198
Glen Jones <gcjones@pilgrimconsultants.com>
2021-01-15 00:04:12 +0000

    Added Password Buffer to edwina ignore

--- init.el
+++ init.el
@@ -96,7 +96,7 @@
        ( magit +forge )            ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
        ( pass +auth )              ; password manager for nerds
-       ;;pdf               ; FIXME doom compile breaks pdf enhancements
+       pdf               ; FIXME doom compile breaks pdf enhancements
        prodigy           ; FIXME managing external services & code builders
        rgb               ; creating color strings
        taskrunner        ; taskrunner for all your projects
